11846c0d8f63c1ba4869ea6eba92e4442a87fe0b
mahdi1079 <mahdi.king.1079@gmail.com>
2025-01-24 10:08:11 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,29 @@ include("header.html");
 
 <section class=" container m-1">
 
+
+
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner m-0">
+        <div class="carousel-item active">
+          <img src="4/dfgfd.png" class="d-block w-100" alt="...">
+          
+        </div>
+        <div class="carousel-item">
+          <img src="4/Untitled-2.png" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+
 <div class=" px-4 text-center m-1">
 
 </div>
